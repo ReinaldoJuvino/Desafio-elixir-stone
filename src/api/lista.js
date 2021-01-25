e52df560389c0308256229b50ec9353375842985
Reinaldo Juvino <reinaldojuvinoneto@gmail.com>
2021-01-25 10:13:04 -0300

    Finalizado

--- src/api/lista.js
+++ src/api/lista.js
@@ -7,7 +7,7 @@ module.exports = app => {
         const emails = request.body.emails
 
         const valorTotal = somaItens(itens)
-        console.log(valorTotal)
+
         let quantidadeEmails = emails.length
         if (quantidadeEmails == 0) {
             response.json( { "valorTotal": valorTotal } )
@@ -17,22 +17,17 @@ module.exports = app => {
         const emailsValores = {}
         for (let i = 0; i < emails.length; i++){
             valorIndividual = Math.round(valorIndividual)
-            console.log(valorIndividual)
             email = emails[i]
             emailsValores[email.email] = valorIndividual
         }
-        try {
-            //valorIndividual = Math.floor(valorIndividual)
-            if (valorIndividual * quantidadeEmails != valorTotal) {
-                acrescimoIndividual = valorTotal - (valorIndividual * quantidadeEmails)
-                emailsValores[email.email] = valorIndividual + acrescimoIndividual
-            }
-        } catch (error) {
-            
+        if (valorIndividual * quantidadeEmails != valorTotal) {
+            acrescimoIndividual = valorTotal - (valorIndividual * quantidadeEmails)
+            emailsValores[email.email] = valorIndividual + acrescimoIndividual
         }
+    
         return response.json(emailsValores)
     }
-
+    
     const somaItens = (itens) => {
         let itensValorTotal = 0
         for (let i = 0; i < itens.length; i++){
